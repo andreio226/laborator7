130d7bfc04163154de62d695261cbe055153b051
andrei <andrei.oltean2@stud.ubbcluj.ro>
2025-11-26 17:31:17 +0200

    comit lab8

---
+++ Models/ShopList.cs
@@ -0,0 +1,15 @@
+using SQLite;
+
+namespace Oltean_Andrei_Lab7.Models
+{
+    public class ShopList
+    {
+        [PrimaryKey, AutoIncrement]
+        public int ID { get; set; }
+
+        [MaxLength(250), Unique]
+        public string Description { get; set; }
+        
+        public DateTime Date { get; set; }
+    }
+}
